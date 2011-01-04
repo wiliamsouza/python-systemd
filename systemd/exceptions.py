4cbb3201991a86f6be1b1415968142cf8e0dc8c9
Wiliam Souza <wiliam@mandriva.com>
2011-01-04 14:56:54 -0200

    Added license header

--- systemd/exceptions.py
+++ systemd/exceptions.py
@@ -1,3 +1,22 @@
+#
+# Copyright (c) 2010 Mandriva
+#
+# This file is part of python-systemd.
+#
+# python-systemd is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# python-systemd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 class SystemdError(Exception):
     def __init__(self, error):
         self.name = error.get_dbus_name().split('.')[3]
